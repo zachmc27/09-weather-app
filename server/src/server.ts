5357eef4cbcd77592e65eb85937b95d739c97cd7
zachmc27 <dzdudes1213@gmail.com>
2025-02-18 16:20:14 -0600

    downloaded dependencies, filled out server.ts and htmlRoutes.ts files, and started weatherService.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,26 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
 dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
 
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+app.use(express.static(path.join(__dirname, 'client')))
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use('/', routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,31 +1,62 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 // TODO: Define an interface for the Coordinates object
-
+interface Coordinates {
+  latitude: string;
+  longitude: string;
+}
 // TODO: Define a class for the Weather object
+class Weather {
 
+}
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  baseURL: string;
+  apiKey: string;
+  cityName: string;
+
+  constructor (cityName: string) {
+    this.baseURL = process.env.API_BASE_URL || "";
+    this.apiKey = process.env.API_KEY || "";
+    this.cityName = cityName
+  }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+
+  }
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+
+  }
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string {
+
+  }
   // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates): string {
+
+  }
   // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData() {
+
+  }
   // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates) {
+
+  }
   // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any) {
+
+  }
   // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+
+  }
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    
+  }
 }
 
 export default new WeatherService();
