a1f2ecf10f413c9098ad6c591610e0bd18eef991
zachmc27 <dzdudes1213@gmail.com>
2025-02-20 18:32:06 -0600

    finished historyservice.ts file

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,5 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import fs from "fs";
+import path from 'path';
+
 // TODO: Define a City class with name and id properties
 class City {
   name: string;
@@ -13,18 +15,37 @@ class City {
 
 // TODO: Complete the HistoryService class
 class HistoryService {
+  private filePath = path.resolve(__dirname, '../../db/searchHistory.json');
   // TODO: Define a read method that reads from the searchHistory.json file
   private async read() {
-    const data = await fs.readFile('../../db/searchHistory.json', 'utf-8',  (error, data) =>
-    error ? console.error(error) : data)
-    return data
+    try {
+    const data = await fs.promises.readFile(this.filePath, 'utf-8');
+    const dataParsed = JSON.parse(data)
+    return dataParsed
+    } catch (error) {
+      console.error('Error reading file.')
+      return []
+    }
   }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    try {
+    await fs.promises.appendFile(this.filePath, JSON.stringify(cities))
+    } catch (error) {
+      console.error('Error writing file')
+    }
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    return this.read()
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {
+    const citiesArray = await this.getCities()
+    const newCity = new City(city, uuidv4())
+    citiesArray.push(newCity)
+    this.write(citiesArray)
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
